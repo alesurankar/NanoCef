093fa87fb60630bec619d9fd3858c7729d70ee70
aleurankar <aleurankar@DEV-MACHINE>
2025-07-07 10:09:54 +0200

    vuetify integrated and demo

--- VueCef/src/App.vue
+++ VueCef/src/App.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+const darkMode = ref(false)
 </script>
 
 <template>
-  <h1>BARABA JEDNA</h1>
+  <v-app :theme="darkMode ? 'dark' : 'light'">
+    <v-main>
+      <v-container width="800">
+        <v-card class="mb-3 py-2 px-3">
+          <p>blablabla, blablablablablabla, blablablablablabla, blablablablablabla, blablabla. Bblablabla, blablablablablabla, blablabla</p>
+          <p>Hahaha, ha,hahaHahaha, ha,haha,Hahaha, ha,hahaHahaha, ha,haha. Hahaha, ha,hahaHahaha, ha,haha</p>
+        </v-card>
+        <v-card class="mb-3 py-2 px-3">
+          <p>juhuhuhu, juhu juhuhu, juhuhu. Juhuhuhu, juhu juhuhu, juhuhujuhuhuhu, juhu juhuhu, juhuhu, juhuhuhu, juhu juhuhu, juhuhu. Jjuhuhuhu, juhu juhuhu, juhuhu</p>
+        </v-card>
+        <v-switch 
+          v-model="darkMode"
+          label="Dark Mode">
+        </v-switch>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
-  h1
-  {
-    color: #ADD8E6;
-    font-size: 5rem;
-    font-family: sans-serif;
-  }
 </style>
